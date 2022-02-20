9007b8a850d8749eab23493ef7d713e9c294dd89
koustav-dhar <koustavdhar.31.05@gmail.com>
2022-02-20 17:04:09 +0530

    socket connect

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,34 @@
+import io from 'socket.io-client';
+import { useEffect, useState } from 'react';
 import './Home.css';
 import Room from './Room';
 import SideBar from './SideBar';
 
 const Home = (props)=>{
     const user = props.user;
+    const [socket, setSocket] = useState(null);
+
+    useEffect(() => {
+        const dummySocket = io.connect('http://localhost:8000');
+        setSocket(dummySocket);
+        console.log('socket connected');
+        dummySocket.emit('trying-to-connect', {
+            userID : user._id
+        });
+
+        dummySocket.on('success', () => {
+            console.log('Success at Frontend too.')
+        });
+
+        dummySocket.on('error', () => {
+            // Insert Error Page
+            dummySocket.emit('leaving', {
+                userID : user._id   
+            });
+        });
+
+    }, [user]);
+
 
     console.log(user);
 
