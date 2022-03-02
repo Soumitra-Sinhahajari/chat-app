df9c9bcab70fa5595f8b9b7f5389af1e2ec8631c
Bibhu <soumitrahajari@gmail.com>
2022-03-03 02:57:13 +0530

    Cleaning

--- models/JoinedRoom.js
+++ models/JoinedRoom.js
@@ -1,11 +1,9 @@
 const joinedRoom = class{
     roomName = String;
     roomId = Object;
-    // lastChattedTime = Date;
     constructor(roomId, roomName){
         this.roomId = roomId;
         this.roomName = roomName;
-        // this.lastChattedTime = lastChattedTime;
     }
 };
 
